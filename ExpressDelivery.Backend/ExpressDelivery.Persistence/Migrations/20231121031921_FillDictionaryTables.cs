ddac508027e7398c2ac1fde1be6052fa10cf6bf2
fantom3527 <62473392+fantom3527@users.noreply.github.com>
2023-11-21 06:44:29 +0300

    Добавлена автоматически накатываемая миграция.
    * Накатывается миграция при запуске приложения
    * Автозаполнение словарей и тестовых денных.

---
+++ ExpressDelivery.Backend/ExpressDelivery.Persistence/Migrations/20231121031921_FillDictionaryTables.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpressDelivery.Persistence.Migrations
+{
+    public partial class FillDictionaryTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO ExecutorStatus (Name, Code, IsActual) " +
+                                 "VALUES ('Ожидает заказа', 'wait', 1), " +
+                                         "('Выполняет заказ', 'submitted', 1), " +
+                                         "('Назначается заказ', 'orderselection', 1);");
+
+            migrationBuilder.Sql("INSERT INTO OrderStatus (Name, Code, IsActual) " +
+                                 "VALUES ('Новый', 'new', 1), " +
+                                         "('Ожидает', 'wait', 1), " +
+                                         "('Передано на выполнение', 'submitted', 1), " +
+                                         "('Выполнен', 'completed', 1), " +
+                                         "('Отменен', 'cancelled', 1), " +
+                                         "('Удален', 'deleted', 1);");
+
+            migrationBuilder.Sql("INSERT INTO CargoType (Name, Code, IsActual) " +
+                                 "VALUES ('Крупногабаритные грузы', 'bulky', 1), " +
+                                         "('Ценные грузы', 'valuable', 1), " +
+                                         "('Перечисляемые грузы', 'listed', 1), " +
+                                         "('Техническое оборудование', 'technicalequipment', 1);");
+
+
+            migrationBuilder.Sql("INSERT INTO OrderHistoryMethod (Name, Code, IsActual) " +
+                                 "VALUES ('Создание заказа', 'create', 1)," +
+                                         "('Редактирование заказа', 'edit', 1), " +
+                                         "('Изменение статуса заказа', 'updatestatus', 1), " +
+                                         "('Назначение исполнителя', 'submittedexecution', 1), " +
+                                         "('Удаление заказа', 'delete', 1);");
+
+
+            migrationBuilder.Sql("INSERT INTO Cargo(Id, CargoTypeId, Name, Description) " +
+                                 "VALUES " +
+                                 "('6F9619FF-8B86-D011-B42D-00C04FC964FF', 1, 'CargoName1', 'Description1'), " +
+                                 "('6F9619FF-8B86-D011-B42D-00C04FC964FF', 2, 'CargoName2', 'Description2'), " +
+                                 "('6F9619FF-8B86-D011-B42D-00C04FC964FF', 3, 'CargoName3', 'Description3');");
+
+            migrationBuilder.Sql("INSERT INTO User(Id, Name, Description) " +
+                                 "VALUES " +
+                                 "('6F9619FF-8B86-D011-B42D-00C04FC964FF', 'John Doe', 'Description 1'), " +
+                                 "('6F9619FF-8B86-D011-B42D-00C04FC964FF', 'Jane Smith', 'Description 2'), " +
+                                 "('6F9619FF-8B86-D011-B42D-00C04FC964FF', 'Alice Johnson', 'Description 3');");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- ExpressDelivery.Backend/ExpressDelivery.WebApi/Program.cs
+++ ExpressDelivery.Backend/ExpressDelivery.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using ExpressDelivery.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace ExpressDelivery.WebApi
@@ -20,10 +21,27 @@ namespace ExpressDelivery.WebApi
                 {
                     var context = serviceProvider.GetRequiredService<ExpressDeliveryDbContext>();
                     DbInitializer.Initialize(context);
+                    serviceProvider.GetRequiredService<ExpressDeliveryDbContext>().Database.Migrate();
                 }
                 catch (Exception exception)
                 {
                     Log.Fatal(exception, "An error occurred while app initialization");
+                }
+
+
+            }
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var context = serviceProvider.GetRequiredService<ExpressDeliveryDbContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception exception)
+                {
+                    Log.Fatal(exception, "An error occurred while applying migrations");
                 }
             }
 
