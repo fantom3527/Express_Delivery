d954232dfa3863aae523903e12fa9e5a1211e0a2
fantom3527 <62473392+fantom3527@users.noreply.github.com>
2023-11-22 01:49:37 +0700

    Добавление контроля для OrderHistory.

---
+++ ExpressDelivery.Backend/ExpressDelivery.WebApi/Controllers/OrderHistoryController.cs
@@ -0,0 +1,26 @@
+﻿using ExpressDelivery.Application.Services.Interfaces;
+using ExpressDelivery.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpressDelivery.WebApi.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [Produces("application/json")]
+    [Route("api/{version:apiVersion}/[controller]")]
+    public class OrderHistoryController : BaseController<IOrderHistoryService>
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderHistory>>> GetAll()
+        {
+            return Ok(await Service.GetAll());
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<ActionResult<IEnumerable<OrderHistory>>> GetAllByOrder(Guid orderId)
+        {
+            //
+            return Ok(await Service.GetAllByOrder(orderId));
+        }
+    }
+}
